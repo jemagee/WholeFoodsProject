dd3064076799433cdc1ddab8d9a7b2e638d0646c
John Magee <jemagee@cox.net>
2017-08-19 15:31:22 -0700

    Simple model specs for brand model, testing included

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,6 @@
 class Brand < ApplicationRecord
   belongs_to :company
+
+  validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3}
+  validates :prefix, presence: true, numericality: true
 end
--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
   it {should belong_to(:company)}
+  it {should validate_presence_of(:name)}
+  it {should validate_length_of(:name).is_at_least(3)}
+  it {should validate_uniqueness_of(:name).case_insensitive}
+  it {should validate_presence_of(:prefix)}
+  it {should validate_numericality_of(:prefix)}
 end
